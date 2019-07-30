7f7bcdb410a990826ce34d3391b5d7dccda3046a
Ronillo Apas Jr <jexos26@gmail.com>
2019-07-30 11:05:51 +0800

    Company Table

---
+++ database/migrations/2019_07_28_230809_create_company_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('company',225);
+            $table->string('street', 100);
+            $table->string('city',50);
+            $table->string('state',5);
+            $table->string('zip',10);
+            $table->string('tracking-phone',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
